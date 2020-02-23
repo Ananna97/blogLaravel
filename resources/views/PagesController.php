e216c361ada327463d4b3477db0bc474456a6c19
Ananna97 <38731739+Ananna97@users.noreply.github.com>
2020-02-23 06:10:41 +0200

    Create Controller.

---
+++ resources/views/PagesController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers; 
+
+use PagesController extends Controller {
+
+	public function getIndex() {
+
+	} 
+
+	public function getAbout() {
+
+	}
+
+	public function getContact() {
+		
+	}
+
+	public function getHome() {
+		
+	}
+}

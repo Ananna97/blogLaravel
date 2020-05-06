9670e7daa4bf95b633dad7b6336060bfea4dc778
Ananna97 <38731739+Ananna97@users.noreply.github.com>
2020-05-07 02:52:45 +0300

    .

--- resources/views/partials/_head.blade.php
+++ resources/views/partials/_head.blade.php
@@ -2,8 +2,8 @@
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 
-<title>Laravel Blog @yield('title')</title>
-
+<title>Octarine - Color of Magic @yield('title')</title>
+<link rel="icon" href="favicon.png" type="image/png" sizes="16x16">
 <!-- Bootstrap -->
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
 {{ Html::style('css/styles.css') }}
